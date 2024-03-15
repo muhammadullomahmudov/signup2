785a8a753ee507dbdc5412c4e65fe85e4e7856f2
UZBEK BOY <muhammadullomakhmudov@gmail.com>
2024-03-15 16:37:56 +0500

    Initial commit

--- script.js
+++ script.js
@@ -18,32 +18,32 @@ const forminner = document.querySelector('.forminner');
 
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault;
+    e.preventDefault();
     if(!email.value.includes("@gmail.com")) {
         text.textContent = "Iltimos @gmail.com qo'ying"
         errorbox.style.display = 'block'
         email.classList.add('red')
         email.classList.add('animation')
-        password.classList.add('animation')
     }
     else {
         text.textContent = ""
         errorbox.style.display = "none"
         email.classList.remove('red')
+        email.classList.remove('animation')
         
     }
 
-    if(password.value.length < 'aaaa'.length) {
+    if(password.value.length < 'aaa'.length) {
         text2.textContent = "Parol kamida 3ta harifli bo'lishi kerak"
         errorbox2.style.display = 'block'
         password.classList.add('red')
-        .classList.add('animation')
         password.classList.add('animation')
     }
     else {
         text2.textContent = ""
         errorbox2.style.display = "none"
         password.classList.remove('red')
+        password.classList.remove('animation')
         
     }
 
